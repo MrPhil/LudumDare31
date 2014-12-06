708f66412b61c4435586459aa53a2fb7d2bffec1
Philip 'MrPhil' Ludington <mrphil@mrphilgames.com>
2014-12-05 21:54:56 -0800

    Added Loading image from disk.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <SDL.h>
+#include <SDL_image.h>
 
 // Setup SDL2
 
 const char WindowTitle[25] = "MrPhil's Ludum Dare 31";
 
-int main(int something, char *args[])
+int main(int argc, char *argv[])
 {
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
 	{
@@ -24,15 +25,39 @@ int main(int something, char *args[])
 
 		if (window)
 		{
+			// Initialize the Image reader lib (PNG)
+			if (IMG_Init(IMG_INIT_PNG) != IMG_INIT_PNG) 
+			{
+				SDL_Log(IMG_GetError());
+			}
+			else
+			{
+				// Now an image
+				char imagePath[] = ".\\Data\\Images\\HelloWorld.png";
+
+				// Load Image from File
+				SDL_Surface *image;
+				image = IMG_Load(imagePath);
+				if (image) 
+				{
+					SDL_Log("Success!");
+				}
+				else
+				{
+					SDL_Log(IMG_GetError());
+				}
+
+				// Shutdown the Image Reader
+				IMG_Quit();
+			}
+
 			// Now a message box
 			SDL_ShowSimpleMessageBox(0,
 				WindowTitle,
-				"Hello World!",
+				"All Done!",
 				window);
 
-			// Now an image
-			char *imagePath = ".\\Data\\Images\\HelloWorld.png";
-
+			// bye bye Window
 			SDL_DestroyWindow(window);
 		}
 	
