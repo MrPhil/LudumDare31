2e666c7a5dccfd0672ec47ab3a2d1cc098682e06
Philip 'MrPhil' Ludington <mrphil@mrphilgames.com>
2014-12-06 00:11:27 -0800

    Clear with MrPhil Green

--- main.cpp
+++ main.cpp
@@ -16,51 +16,105 @@ int main(int argc, char *argv[])
 		SDL_Log("Hello World.");
 
 		// We need a Window!
-		SDL_Window *window = SDL_CreateWindow(WindowTitle,
-			50,
-			50,
+		SDL_Window *window = SDL_CreateWindow(
+			WindowTitle,
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
 			1024,
 			800,
-			0);
+			0); // Window Flags
 
-		if (window)
+		if (window != NULL)
 		{
-			// Initialize the Image reader lib (PNG)
-			if (IMG_Init(IMG_INIT_PNG) != IMG_INIT_PNG) 
-			{
-				SDL_Log(IMG_GetError());
-			}
-			else
-			{
-				// Now an image
-				char imagePath[] = ".\\Data\\Images\\HelloWorld.png";
+			// Get our Renderer
+			SDL_Renderer* renderer = SDL_CreateRenderer(
+				window, 
+				-1, // Index of the rendering driver to initialize (-1 for first supporting)
+				0); // Renderer Flags
 
-				// Load Image from File
-				SDL_Surface *image;
-				image = IMG_Load(imagePath);
-				if (image) 
+			if (renderer != NULL)
+			{
+				// Initialize the Image reader lib (PNG)
+				if (IMG_Init(IMG_INIT_PNG) == IMG_INIT_PNG)
 				{
-					SDL_Log("Success!");
+					// Now an image
+					char imagePath[] = ".\\Data\\Images\\HelloWorld.png";
+
+					// Load Image from File
+					SDL_Surface *surface;
+					surface = IMG_Load(imagePath);
+					if (surface != NULL)
+					{
+						// Get the Texture from the Surface (our PNG)
+						SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+						if (texture != NULL)
+						{
+							// Let's clear out the Renderer with MrPhil Green
+							SDL_SetRenderDrawColor(renderer, 130, 198, 74, 255);
+
+							//Clear screen
+							SDL_RenderClear(renderer);
+
+							//// 
+							//SDL_RenderCopy(renderer, texture, NULL, NULL);
+
+							// Show use what we've done!
+							SDL_RenderPresent(renderer);
+
+							// Give us time to see the window.
+							SDL_Delay(5000);
+
+							// Now a message box
+							SDL_ShowSimpleMessageBox(0,
+								WindowTitle,
+								"Success!",
+								window);
+
+							SDL_DestroyTexture(texture);
+						}
+						else
+						{
+							SDL_Log("FAILED: SDL_CreateTextureFromSurface");
+							SDL_Log(SDL_GetError());
+						}
+
+						// We are done with the surface now
+						SDL_FreeSurface(surface);
+					}
+					else
+					{
+						SDL_Log("FAILED: IMG_Load");
+						SDL_Log(SDL_GetError());
+					}
+
+					// Shutdown the Image Reader
+					IMG_Quit();
 				}
 				else
 				{
-					SDL_Log(IMG_GetError());
+					SDL_Log("FAILED: IMG_Init");
+					SDL_Log(SDL_GetError());
 				}
 
-				// Shutdown the Image Reader
-				IMG_Quit();
+				// Get rid of our Renderer
+				SDL_DestroyRenderer(renderer);
 			}
-
-			// Now a message box
-			SDL_ShowSimpleMessageBox(0,
-				WindowTitle,
-				"All Done!",
-				window);
+			else
+			{
+				SDL_Log("FAILED: SDL_GetRenderer");
+				SDL_Log(SDL_GetError());
+			}
 
 			// bye bye Window
 			SDL_DestroyWindow(window);
 		}
-	
+		else
+		{
+			SDL_Log("FAILED: SDL_CreateWindow");
+			SDL_Log(SDL_GetError());
+		}
+
 		// All Done
 		SDL_Quit();
 	}
