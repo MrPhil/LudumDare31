eaed02f7928ee0a5a5832c5662dae9fb16c02730
Philip 'MrPhil' Ludington <mrphil@mrphilgames.com>
2014-12-05 20:16:13 -0800

    Got SDL_Window and MessageBox working

--- main.cpp
+++ main.cpp
@@ -2,11 +2,32 @@
 
 // Setup SDL2
 
+const char WindowTitle[25] = "MrPhil's Ludum Dare 31";
+
 int main(int something, char *args[])
 {
-	SDL_Init(SDL_INIT_EVERYTHING);
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		SDL_Log(SDL_GetError());
+	}
+	else
+	{
+		SDL_Log("Hello World.");
 
-	SDL_Log("Hello World.");
+		// We need a Window!
+		SDL_Window *window = SDL_CreateWindow(WindowTitle,
+			50,
+			50,
+			1024,
+			800,
+			0);
+
+		SDL_ShowSimpleMessageBox(0, 
+			WindowTitle,
+			"Hellow World!",
+			window);
+	}
+
 
 	return 0;
 }
